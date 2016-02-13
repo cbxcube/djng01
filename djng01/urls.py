413e0ec08c8540d72929584fb7021d5efc779021
cbxcube <cubbed@gmail.com>
2016-02-13 14:10:23 +0100

    updated settings, app01 created, south for db migration added, django admin enabled

--- djng01/urls.py
+++ djng01/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+#from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+ from django.contrib import admin
+ admin.autodiscover()
 
-urlpatterns = patterns('',
+#urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'djng01.views.home', name='home'),
     # url(r'^djng01/', include('djng01.foo.urls')),
@@ -13,5 +13,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+      url(r'^admin/', include(admin.site.urls)),
 )
